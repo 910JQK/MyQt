69a72a0bd609b8e30d68f8e1889bcdff4df9fe24
910JQK <v910JQK@gmail.com>
2014-03-16 02:38:06 +0800

    140316

---
+++ SqlBrowser/locale/sqlbrowser_zh_CN.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="47"/>
+        <source>Tables</source>
+        <translation>数据表</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="55"/>
+        <source>Open file...</source>
+        <translation>打开文件…</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="58"/>
+        <source>Save changes of current table</source>
+        <translation>保存当前表的修改</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="62"/>
+        <source>Revert all changes of the current table</source>
+        <translation>回退当前表的修改</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="65"/>
+        <source>Revert all changes of the current table and refresh data from database</source>
+        <translation>回退当前表的修改并从数据库刷新数据</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="68"/>
+        <source>Add a row record to the current table</source>
+        <translation>新增一条行记录</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="71"/>
+        <source>Delete selected row-records from the current table</source>
+        <translation>删除选中的行记录</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="81"/>
+        <source>Toolbar</source>
+        <translation>工具栏</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="91"/>
+        <source>Input SQL command here</source>
+        <translation>在这里输入SQL命令</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="100"/>
+        <location filename="../sqlbrowser.cpp" line="312"/>
+        <location filename="../sqlbrowser.cpp" line="334"/>
+        <source>Sqlbrowser</source>
+        <translation>Sqlbrowser</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="173"/>
+        <source>Error:Not enough arguments to open the Mysql database</source>
+        <translation>错误：没有足够的参数来打开MySQL数据库</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="221"/>
+        <source>PORT</source>
+        <translation>端口</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="222"/>
+        <source>HOST</source>
+        <translation>主机</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="223"/>
+        <source>USERNAME</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="224"/>
+        <source>DBNAME</source>
+        <translation>数据库名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="218"/>
+        <source>DBFILE</source>
+        <translation>数据库文件</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="217"/>
+        <source>optional arguments:</source>
+        <translation>可选参数：</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="218"/>
+        <source>Sqlite3 database file to be opened</source>
+        <translation>要打开的Sqlite3数据库文件</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="219"/>
+        <source>Show this help message and exit</source>
+        <translation>显示此帮助信息并退出</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="220"/>
+        <source>Specify database type : Mysql</source>
+        <translation>指明数据库类型：MySQL</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="221"/>
+        <source>Port of Mysql connection, 3306 for default</source>
+        <translation>MySQL端口，默认3306</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="222"/>
+        <source>Host of Mysql connection</source>
+        <translation>MySQL主机/服务器</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="223"/>
+        <source>Username of Mysql connection</source>
+        <translation>MySQL用户名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="224"/>
+        <source>Database of Mysql connection</source>
+        <translation>MySQL数据库名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="241"/>
+        <source>Password:</source>
+        <translation>密码：</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../sqlbrowser.cpp" line="584"/>
+        <source>SqlQuery : %n row(s) affected.</source>
+        <translation>
+            <numerusform>SqlQuery：%n行受到影响</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
---
+++ SqlBrowser/locale/sqlbrowser_zh_TW.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="47"/>
+        <source>Tables</source>
+        <translation>資料表</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="55"/>
+        <source>Open file...</source>
+        <translation>開啓檔案…</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="58"/>
+        <source>Save changes of current table</source>
+        <translation>儲存當前表的修改</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="62"/>
+        <source>Revert all changes of the current table</source>
+        <translation>回退當前表的修改</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="65"/>
+        <source>Revert all changes of the current table and refresh data from database</source>
+        <translation>回退當前表的修改並從資料庫刷新資料</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="68"/>
+        <source>Add a row record to the current table</source>
+        <translation>新增一條列記錄</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="71"/>
+        <source>Delete selected row-records from the current table</source>
+        <translation>刪除選中的列記錄</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="81"/>
+        <source>Toolbar</source>
+        <translation>工具列</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="91"/>
+        <source>Input SQL command here</source>
+        <translation>在這裡輸入SQL指令</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="100"/>
+        <location filename="../sqlbrowser.cpp" line="312"/>
+        <location filename="../sqlbrowser.cpp" line="334"/>
+        <source>Sqlbrowser</source>
+        <translation>Sqlbrowser</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="173"/>
+        <source>Error:Not enough arguments to open the Mysql database</source>
+        <translation>錯誤：沒有足夠的參數來開啓MySQL資料庫</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="221"/>
+        <source>PORT</source>
+        <translation>埠</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="222"/>
+        <source>HOST</source>
+        <translation>主機</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="223"/>
+        <source>USERNAME</source>
+        <translation>使用者</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="224"/>
+        <source>DBNAME</source>
+        <translation>資料庫名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="216"/>
+        <location filename="../sqlbrowser.cpp" line="218"/>
+        <source>DBFILE</source>
+        <translation>資料庫檔案</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="217"/>
+        <source>optional arguments:</source>
+        <translation>可選參數：</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="218"/>
+        <source>Sqlite3 database file to be opened</source>
+        <translation>要打開的Sqlite3資料庫檔案</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="219"/>
+        <source>Show this help message and exit</source>
+        <translation>顯示此求助說明並離開</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="220"/>
+        <source>Specify database type : Mysql</source>
+        <translation>指明資料庫類型：MySQL</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="221"/>
+        <source>Port of Mysql connection, 3306 for default</source>
+        <translation>MySQL埠，默認3306</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="222"/>
+        <source>Host of Mysql connection</source>
+        <translation>MySQL主機/伺服器</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="223"/>
+        <source>Username of Mysql connection</source>
+        <translation>MySQL使用者</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="224"/>
+        <source>Database of Mysql connection</source>
+        <translation>MySQL資料庫名</translation>
+    </message>
+    <message>
+        <location filename="../sqlbrowser.cpp" line="241"/>
+        <source>Password:</source>
+        <translation>密碼：</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../sqlbrowser.cpp" line="584"/>
+        <source>SqlQuery : %n row(s) affected.</source>
+        <translation>
+            <numerusform>SqlQuery：%n列受到影響</numerusform>
+        </translation>
+    </message>
+</context>
+</TS>
